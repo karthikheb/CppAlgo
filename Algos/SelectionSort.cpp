5446147f7588ada475ddbdb74b77fe99d8f5c96d
Karthik Hebbar <hebbav@gmail.com>
2012-11-02 02:25:29 +0530

    SelectionSort

---
+++ Algos/SelectionSort.cpp
@@ -0,0 +1,27 @@
+template<class T>
+inline void swapItem(T& a, T& b)
+{
+	T temp = a;
+	a = b;
+	b = temp;
+}
+
+template<class T>
+void selectionSort( int size, T* items)
+{
+	for(int count=0; count < size - 1; ++count)
+	{
+		int minIndex = count;
+		T* minValue = items + count;
+
+		for(int index = count + 1; index < size; ++index)
+		{
+			if(items[index] < *(minValue))
+			{
+				minIndex = index;
+				minValue = items + index;
+			}
+		}
+		swapItem(*(items+count), *minValue);
+	}
+}
