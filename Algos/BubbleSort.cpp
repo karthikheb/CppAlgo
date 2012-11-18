db0807b43191ccd0b2816d00fe487b3422ed1c5e
Karthik Hebbar <hebbav@gmail.com>
2012-11-18 23:33:58 +0530

    Modifications to make it look better

--- Algos/BubbleSort.cpp
+++ Algos/BubbleSort.cpp
@@ -1,13 +1,17 @@
+//Bubble Sort cpp implementation
+//Takes in array of elements T and size of array as parameters
+//T should implement '>' operator
+
 template<class T>
 inline void swapItem(T& a, T& b)
 {
-	T temp = a;
+	T temp(a);
 	a = b;
 	b = temp;
 }
 
 template<class T>
-void bubbleSort( int size, T* items)
+void bubbleSort( int size, T items[])
 {
 	for(int count=0; count < size - 1; ++count)
 	{
--- Algos/SelectionSort.cpp
+++ Algos/SelectionSort.cpp
@@ -1,13 +1,17 @@
+//Selection Sort cpp implementation
+//Takes in array of elements T and size of array as parameters
+//T should implement '>' operator
+
 template<class T>
 inline void swapItem(T& a, T& b)
 {
-	T temp = a;
+	T temp(a);
 	a = b;
 	b = temp;
 }
 
 template<class T>
-void selectionSort( int size, T* items)
+void selectionSort( int size, T items[])
 {
 	for(int count=0; count < size - 1; ++count)
 	{
